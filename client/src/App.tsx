513e51cc9e025a370b5082c0798d9aaefa0658b8
Charisma <actuallycharisma@outlook.com>
2024-09-04 12:47:17 +0100

    adds metrics endpoint, cors and intervals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,72 @@
+import React, {useEffect, useState,} from 'react';
 import './App.css';
 
-function App() {
+const  App = ()  =>{ 
+  const FETCH_INTERVAL = 30000;
+  const [loading, setLoading] = useState<Boolean>(false)
+  const [serverTime, setSetServerTime] = useState(0)
+  const [timeDiff, setTimeDiff] = useState<string>('')
+  const [metrics, setMetrics] = useState<String>('')
+
+  const fetchTime = async () => {
+    const res = await fetch('http://localhost:8080/time', {
+      method: 'GET',
+      headers: {
+        "Authorization": 'mysecrettoken',       
+        "Content-Type":"application/json",
+        "Accept": "application/json",
+    },
+    })
+
+    if(!res.ok) {
+      console.error(res.status)
+    }
+
+    const timeData = await res.json()
+    setSetServerTime(timeData.properties.epoch.description)
+  }
+
+  const fetchMetrics = async () => {
+    const res = await fetch('http://localhost:8080/metrics', {
+      method: 'GET',
+      headers: {
+        "Authorization": 'mysecrettoken',
+      }
+    })
+    if(!res.ok){
+      console.error(res.status)
+    }
+
+    const metricsData = await res.text()
+    setMetrics(metricsData)
+    
+  }
+
+  useEffect(() => {
+        fetchTime(); 
+        fetchMetrics();
+
+    const fetchInterval = setInterval(() => {
+        fetchTime();
+    }, FETCH_INTERVAL);
+
+        return () => clearInterval(fetchInterval);
+  },[])
+
+
+  if (loading) { return <div>Loading...</div> }
+  
   return (
-    <>
-    </>
+    <div className="App">
+      <div className='grid-container'>
+        <div className='grid-content'>
+      Server time: {new Date(serverTime).toString()}
+    </div>
+    <pre className='grid-content'>
+      {metrics}
+    </pre>
+      </div>
+    </div>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import expressPrometheusMiddleware from 'express-prometheus-middleware';
-import cors from 'cors'
+import cors from 'cors';
 
-const app = express(); 
+const app = express();
 
-app.use(cors()); 
-app.use((req, res, next) => {
-    if(req.headers.authorization !== 'mysecrettoken') {
-        return res.status(403).send('Forbidden'); 
-    }
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-})
+app.use(cors())
+app.use(function(req, res, next) {
+  if (req.headers.authorization !== 'mysecrettoken') {
 
-app.get('/time', (req, res) => {
+    return res.status(403).json({ error: 'No credentials sent!' });
+  }
+ res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  next();
+});
+
+app.get("/time", (req, res) => {
     res.send({
         properties: {
             epoch: {
@@ -24,12 +27,11 @@ app.get('/time', (req, res) => {
         required: ['epoch'],
         type: 'object'
     });
+});
 
-    app.use(expressPrometheusMiddleware({
-        metricsPath: '/metrics',
-        collectDefaultMetrics: true
-    }))
+app.use(expressPrometheusMiddleware({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true
+}))
 
-    }); 
-
-    app.listen(8080); 
+app.listen(8080);
