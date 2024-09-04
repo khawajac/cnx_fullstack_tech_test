80badbd544c5490b5d4c1ac3b27ca7a217e203e7
Charisma <actuallycharisma@outlook.com>
2024-09-04 11:47:30 +0100

    adds get time endpoint in required schema format

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
+
+
 const app = express (); 
+var express = require('express'); 
+
+app.get('/time', (requ, res) => {
+    res.send({
+        properties: {
+            epoch: {
+                description: Date.now(),
+                type: 'number'
+            }
+        },
+        required: ['epoch'],
+        type: 'object'
+    });
+
+    }); 
