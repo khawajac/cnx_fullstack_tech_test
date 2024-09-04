c5a40b5fcadb273a9df6b737077ed15aab017c34
Charisma <actuallycharisma@outlook.com>
2024-09-04 12:09:23 +0100

    created initial client side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import expressPrometheusMiddleware from 'express-prometheus-middleware';
-
+import cors from 'cors'
 
 const app = express(); 
 
+app.use(cors()); 
 app.use((req, res, next) => {
     if(req.headers.authorization !== 'mysecrettoken') {
         return res.status(403).send('Forbidden'); 
@@ -12,7 +13,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 })
 
-app.get('/time', (requ, res) => {
+app.get('/time', (req, res) => {
     res.send({
         properties: {
             epoch: {
