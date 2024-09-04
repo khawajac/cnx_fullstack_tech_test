df08a0df994cab38341721b0e36a9f4824f7ba11
Charisma <actuallycharisma@outlook.com>
2024-09-04 12:04:55 +0100

    added auth header requirements

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import expressPrometheusMiddleware from 'express-prometheus-middleware';
+
 
 const app = express(); 
+
+app.use((req, res, next) => {
+    if(req.headers.authorization !== 'mysecrettoken') {
+        return res.status(403).send('Forbidden'); 
+    }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 
 app.get('/time', (requ, res) => {
     res.send({
@@ -13,6 +23,11 @@ app.get('/time', (requ, res) => {
         required: ['epoch'],
         type: 'object'
     });
+
+    app.use(expressPrometheusMiddleware({
+        metricsPath: '/metrics',
+        collectDefaultMetrics: true
+    }))
 
     }); 
 
